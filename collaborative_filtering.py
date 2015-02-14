fdeaf94d059d0b9ff3ac7908683b3fb1512a8e0a
Niklas Weber <nalderan@gmail.com>
2015-02-14 18:22:53 +0100

    calc_distances and its tests

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,1 +1,29 @@
 __author__ = 'niklas'
+
+import pandas as pd
+import math
+
+
+def calc_distances(ratings_frame, uid):
+    """
+    calculate euclidean distances from this user
+    :param ratings_frame: pd.Frame, index = itemId, columns = users
+    :param uid:
+    :return: pd.Series, index = user id, value = distance
+    """
+    user = ratings_frame[uid]
+    dists_dict = {column: math.sqrt(((series - user)**2).sum()) for column, series in ratings_frame.iteritems()}
+    return pd.Series(dists_dict)
+
+
+# find the k closest users to a given user u
+
+def find_k_closest_users(ratings_frame, uid, k):
+    distance_series = calc_distances(ratings_frame, uid)
+    distance_series = distance_series.order(ascending=False)
+    assert len(distance_series) >= k
+    return distance_series.index[0:(k + 1)]
+
+
+if __name__ == "__main__":
+    pass
--- collaborative_filtering_test.py
+++ collaborative_filtering_test.py
@@ -1,1 +1,33 @@
 __author__ = 'niklas'
+
+import unittest
+import collaborative_filtering as cf
+import pandas as pd
+import math
+
+
+class UserDistanceTest(unittest.TestCase):
+    def test_calc_distances(self):
+        u1_ratings = [1, 2, 3]
+        u2_ratings = [1, 2, 3]
+        u3_ratings = [2, 3, 4]
+        ratings_dict = {0: u1_ratings,
+                        1: u2_ratings,
+                        2: u3_ratings}
+        ratings_frame = pd.DataFrame(ratings_dict)
+        dists = cf.calc_distances(ratings_frame, 0)
+        expected = pd.Series([0, 0, math.sqrt(3)])
+        self.assertTrue(expected.equals(dists))
+
+# class AccuracyTest(unittest.TestCase):
+# def testAccuray(self):
+#         """"accuracy is calculated correctly"""
+#         predicted = pd.Series([0, 0, 1, 1, 1])
+#         real = pd.Series([0, 1, 0, 1, 1])
+#         self.assertEqual(nb.calc_accuracy(predicted, real), 3 / 5)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
